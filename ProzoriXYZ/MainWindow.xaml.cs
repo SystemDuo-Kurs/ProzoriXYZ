5a15bf1f8a0dd619bf35b65cb260e2463f510b2b
Dragan Pepic <d.pepic@hotmail.com>
2021-09-06 18:26:30 +0200

    Add project files.

---
+++ ProzoriXYZ/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProzoriXYZ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Osoba o = new();
+            DataContext = o;
+
+            List<Osoba> osobe = new();
+            osobe.Add(new Osoba { Ime = "Pera", Prezime = "Peric" });
+            osobe.Add(new Osoba { Ime = "Neko", Prezime = "Nekic" });
+            osobe.Add(new Osoba { Ime = "Asd", Prezime = "Zklj" });
+            dg.ItemsSource = osobe;
+
+            string neki = "asd";
+            string asd = null;
+            int br = 45;
+            int? brrr = null;
+        }
+    }
+}
